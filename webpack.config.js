3066881d78714f87e190e8032e96280ee1161799
Ilona019 <ilona.arefieva91119@yandex.ru>
2019-08-05 22:49:29 +0300

    scss base files, pug  layout

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const config = {
 		filename: "[name].min.js"
     },
     
-	devtool: isProduction ? false : "source-map",
+	devtool: isProduction ? false : "source-map",//складываем в один файл скрипты и стили, он растёт, в браузере указание на маленьки кусочки, где стиль или срипт лежит, для отладки в режиме разработки.
 	module: {
 		rules: [
 			{
@@ -18,7 +18,7 @@ const config = {
 				use: {
 					loader: "babel-loader",
 					options: {
-						presets: ["@babel/preset-env"]
+						presets: ["@babel/preset-env"]//могли созать отдельный файл в корне babel.config.js
 					}
 				}
 			}
