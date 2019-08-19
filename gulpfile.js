73c48a1eadbe523e1b654d4b5a0bb6584f734b15
Ilona019 <ilona.arefieva91119@yandex.ru>
2019-08-19 22:44:39 +0300

    new task icons

--- gulpfile.js
+++ gulpfile.js
@@ -1,134 +1,119 @@
-//common modules
+// common
 const gulp = require("gulp");
 const plumber = require("gulp-plumber");
 const gulpIf = require("gulp-if");
-//clear
+
+// clear task
 const del = require("del");
 
-//templates
-const pug = require('gulp-pug');
+// pug task
+const pug = require("gulp-pug");
 
-//scripts
+// scss task
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+const minifyCss = require("gulp-clean-css");
+
+//js
 const webpack = require("webpack");
 const gulpWebpack = require("webpack-stream");
-const webpackConfig = require("./webpack.config");
+const webpackConfig = require("./webpack.config.js");
 
-//image
-const imagemin = require('gulp-imagemin');
-const isProduction = process.env.NODE_ENV === "production";
+//images
+const imagemin = require("gulp-imagemin");
 
-//server
+// server
 const browserSync = require("browser-sync").create();
-
-//styles
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
-const minifyCss = require('gulp-clean-css');
 
 //svg-sprite
 const svgSprite = require("gulp-svg-sprite");
 const svgmin = require("gulp-svgmin");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const PATHS = {
-    app:"./app",
-    dist:"./dist"
-}
+	app: "./app",
+	dist: "./dist"
+};
 
-gulp.task('clear', () => {
-    return del(PATHS.dist);
-});
+gulp.task("clear", () => del(PATHS.dist));
 
-gulp.task('templates', () => {
-    return gulp
-        .src(`${PATHS.app}/pages/**/*.pug`,//с какими файлами работать
-        {since: gulp.lastRun("templates")} )//ошибки
-        .pipe(plumber())
-        .pipe(pug({ pretty:true }))//преобразовали файлы
-        .pipe(gulp.dest(PATHS.dist));//разместили на диске
-});
-
-//конкатенация js скриптов в общий бандл
-gulp.task('scripts', () => {
-    return gulp
-        .src(`${PATHS.app}/common/scripts/**/*.js`,//с какими файлами работать, где брать исходники
-        {since: gulp.lastRun("scripts")} )//ошибки
-        .pipe(plumber())
-        .pipe(gulpWebpack(webpackConfig, webpack))
-        .pipe(gulp.dest(`${PATHS.dist}/assets/scripts`));//зап веппак и вернет скрипты, положить их в папку
+gulp.task("templates", () => {
+	return gulp
+		.src(`${PATHS.app}/pages/**/*.pug`)
+		.pipe(plumber())
+		.pipe(pug({ pretty: true }))
+		.pipe(gulp.dest(PATHS.dist));
 });
 
 gulp.task("styles", () => {
-    return gulp
-    .src(`${PATHS.app}/common/styles/app.scss`, {
-    since: gulp.lastRun("styles")
-    })
-    .pipe(plumber())
-    .pipe(gulpIf(!isProduction, sourcemaps.init()))
-    .pipe(sass())
-    .pipe(autoprefixer())
-    .pipe(gulpIf(isProduction, minifyCss()))
-    .pipe(gulpIf(!isProduction, sourcemaps.write()))
-    .pipe(gulp.dest(`${PATHS.dist}/assets/styles`));
+	return gulp
+		.src(`${PATHS.app}/common/styles/app.scss`)
+		.pipe(plumber())
+		.pipe(gulpIf(!isProduction, sourcemaps.init()))
+		.pipe(sass())
+		.pipe(autoprefixer())
+		.pipe(gulpIf(isProduction, minifyCss()))
+		.pipe(gulpIf(!isProduction, sourcemaps.write()))
+		.pipe(gulp.dest(`${PATHS.dist}/assets/styles`));
+});
+
+gulp.task("scripts", () => {
+	return gulp
+		.src(`${PATHS.app}/common/scripts/*.js`, { since: gulp.lastRun("scripts") })
+		.pipe(plumber())
+		.pipe(gulpWebpack(webpackConfig, webpack))
+		.pipe(gulp.dest(`${PATHS.dist}/assets/scripts`));
 });
 
 gulp.task("images", () => {
-    return gulp
-    .src(`${PATHS.app}/common/images/**/*.+(png|jpg|jpeg|gif|svg|ico)`, {
-   // since: gulp.lastRun("images")
-    })
-    .pipe(plumber())
-    .pipe(gulpIf(isProduction, imagemin()))
-    .pipe(gulp.dest(`${PATHS.dist}/assets/images`));
+	return gulp
+		.src(`${PATHS.app}/common/images/**/*.+(png|jpg|jpeg|gif|svg|ico)`)
+		.pipe(plumber())
+		.pipe(gulpIf(isProduction, imagemin()))
+		.pipe(gulp.dest(`${PATHS.dist}/assets/images`));
 });
 
-gulp.task("server", () => {
-    browserSync.init({
-    server: PATHS.dist
-    });
-    browserSync.watch(PATHS.dist + "/**/*.*").on("change", browserSync.reload);
-});
-
-gulp.task("watch", () => {
-    gulp.watch(`${PATHS.app}/**/*.pug`, gulp.series("templates"));
-    gulp.watch(`${PATHS.app}/**/*.scss`, gulp.series("styles"));
-    gulp.watch(`${PATHS.app}/**/*.js`, gulp.series("scripts"));
-    gulp.watch(
-    `${PATHS.app}/common/images/**/*.+(png|jpg|jpeg|gif|svg|ico)`,
-    gulp.series("images")
-    );
- });
-
- gulp.task('copy', async() => {
-    var fonts = gulp.src('app/common/fonts/**/*')
-    .pipe(gulp.dest('dist/assets/fonts/'))
+gulp.task("copy", () => {
+	return gulp
+		.src(`${PATHS.app}/common/fonts/**/*`)
+		.pipe(plumber())
+		.pipe(gulp.dest(`${PATHS.dist}/assets/fonts`));
 });
 
 gulp.task("icons", () => {
-  return gulp
-    .src(`${PATHS.app}/common/icons/**/*.svg`)
-    .pipe(plumber())
-    .pipe(svgmin({
-      js2svg: {
-        pretty: true
-      }
-    }))
-    .pipe(
-      svgSprite({
-        mode: {
-          symbol: {//формир-ся спрайт в виде иконки стиля
-            sprite: "../dist/assets/images/icons/sprite.svg",//итоговый спрайт
-            render: {
-              scss: {
-                dest:'../app/common/styles/helpers/sprites.scss',
-                template: './app/common/styles/helpers/sprite-template.scss'//сформир стиль по шаблону в helpers/sprite-template.scss
-              }
-            }
-          }
-        }
-      })
-    )
-    .pipe(gulp.dest('./'));
+	return gulp
+		.src(`${PATHS.app}/common/icons/**/*.svg`)
+		.pipe(plumber())
+		.pipe(svgmin({
+			js2svg: {
+				pretty: true
+			}
+		}))
+		.pipe(
+			svgSprite({
+				mode: {
+					symbol: {
+						sprite: "../dist/assets/images/icons/sprite.svg",
+						render: {
+							scss: {
+								dest:'../app/common/styles/helpers/sprites.scss',
+								template: './app/common/styles/helpers/sprite-template.scss'
+							}
+						}
+					}
+				}
+			})
+		)
+		.pipe(gulp.dest('./'));
+});
+
+gulp.task("server", () => {
+	browserSync.init({
+		server: PATHS.dist
+	});
+	browserSync.watch(PATHS.dist + "/**/*.*").on("change", browserSync.reload);
 });
 
 gulp.task("watch", () => {
@@ -139,20 +124,25 @@ gulp.task("watch", () => {
 		`${PATHS.app}/common/images/**/*.+(png|jpg|jpeg|gif|svg|ico)`,
 		gulp.series("images")
 	);
+	gulp.watch(
+		`${PATHS.app}/common/icons/**/*.svg`,
+		gulp.series("icons")
+	);
 });
 
- gulp.task(
-     "default",
-     gulp.series(      
-         gulp.parallel("templates", "icons", "styles", "scripts", "images", "copy"),
-         gulp.parallel("watch", "server")
-     )
- )
+gulp.task(
+	"default",
+	gulp.series(
+		"icons",
+		gulp.parallel("templates", "styles", "scripts", "images", "copy"),
+		gulp.parallel("watch", "server")
+	)
+);
 
- gulp.task(
-     "production",
-     gulp.series(
-         "clear",
-         gulp.parallel("templates", "icons", "styles", "scripts", "images")
-     )
- )
+gulp.task(
+	"production",
+	gulp.series(
+		"clear",
+		gulp.parallel("templates", "icons", "styles", "scripts", "images",  "copy")
+	)
+);
